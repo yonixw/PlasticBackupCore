af95e36fb85bc3aa97535b8b2d41d89c7a105b3d
yonixw <yonixw@gmail.com>
2017-11-22 20:49:40 +0200

    Trying to make connection work for testing

---
+++ PlasticBackupSQLiteDB.Test/UnitTestMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlasticBackupDB.SQLTables;
+using PlasticBackupSQLiteDB;
+
+namespace PlasticBackupSQLiteDB.Test
+{
+    [TestClass]
+    public class UnitTestMain
+    {
+        public static string GetRelativePath(string relativePath)
+        {
+            //https://stackoverflow.com/q/10204091/1997873
+            return
+                System.IO.Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                relativePath);
+        }
+
+        [TestMethod]
+        public void TestConnection()
+        {
+            SQLConnection conn = new SQLConnection(
+                "PlasticBackupSQLiteDB_Debug.sqlite3"
+                );
+            conn.Open();
+
+            
+
+            conn.Close();
+        }
+    }
+}
