58aeb82db3fe8ce09a08406d1f31db36e08d8f11
yonixw <yonixw@gmail.com>
2017-11-23 01:06:34 +0200

    each file will have it own SQL queries

--- PlasticBackupDB/SQLData/FolderTree.cs
+++ PlasticBackupDB/SQLData/FolderTree.cs
@@ -7,6 +7,15 @@ namespace PlasticBackupDB.SQLData
 {
     public class FolderTree : SQLUtils.SQLFunctions
     {
+        public FolderTree(SQLUtils.SQLConnection conn)
+        {
+            // Prepare the commands:
+            SQL_FOLDERTREE_insert.myConnection = conn;
+            SQL_FOLDERTREE_selectById.myConnection = conn;
+            SQL_FOLDERTREE_selectByParentAndName.myConnection = conn;
+            SQL_FOLDERTREE_lastSequence.myConnection = conn;
+        }
+
         public class FolderTreeRow
         {
             public int id = -1;
@@ -17,8 +26,152 @@ namespace PlasticBackupDB.SQLData
 
         public FolderTreeRow createOrFindChildFolder(FolderTreeRow parentFolder, string newName) { return null; }
 
-        public FolderTreeRow createOrFindFolder(List<string> pathList) { return null; }
+        public FolderTreeRow createOrFindFolder(List<string> pathList)
+        {
+            // For each part, find o.w. insert and continue.
+            FolderTreeRow lastFolder = new FolderTreeRow();
+           
+            foreach (string folder in pathList)
+            {
+                lastFolder = findFolderByParentAndName(lastFolder.parentid, folder);
+
+                if (!lastFolder.error)
+                {
+                    // folder exist! next one!
+                }
+                else
+                {
+                    // Create and get it!
+                    lastFolder = newFolder(lastFolder.id, folder);
+                }
+            }
+
+            return lastFolder;
+        }
 
         public List<FolderTreeRow> getSubFolders(FolderTreeRow folder) { return null; }
+
+
+        // ************************
+        // FolderTree SQL
+        // ************************
+
+        public SQLUtils.SQLCommand SQL_FOLDERTREE_insert =
+            new SQLUtils.SQLCommand(
+                @"INSERT INTO FolderTree (parentid, name) VALUES (@parentid, @name)",
+                new List<SQLUtils.SQLCommand.SQLParam>()
+                {
+                    new SQLUtils.SQLCommand.SQLParam("@parentid", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
+                    new SQLUtils.SQLCommand.SQLParam("@name", SQLUtils.SQLCommand.SQLParam.sqliteType.TEXT)
+                });
+
+        FolderTreeRow newFolder(int parentid, string partname)
+        {
+            FolderTreeRow result = new FolderTreeRow();
+
+            // Insert new:
+            int resultCode = SQL_FOLDERTREE_insert.ExecuteNonScalar(
+                    new List<object>() { parentid, partname }
+                );
+
+            // Get last index:
+            int folderId = getLastSequence();
+
+            // Return folder:
+            result = findFolderByID(folderId);
+
+            return result;
+        }
+
+        public SQLUtils.SQLCommand SQL_FOLDERTREE_selectById =
+            new SQLUtils.SQLCommand(
+                @"SELECT * FROM FolderTree WHERE id = @id",
+                new List<SQLUtils.SQLCommand.SQLParam>()
+                {
+                    new SQLUtils.SQLCommand.SQLParam("@id", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
+                });
+
+        FolderTreeRow findFolderByID(int rowid)
+        {
+            FolderTreeRow result = new FolderTreeRow();
+
+            List<FolderTreeRow> rows = new List<FolderTreeRow>();
+            rows = SQL_FOLDERTREE_selectById.ExecuteReadAll<FolderTreeRow>(
+                    new List<object>() { rowid },
+                    (reader) => {
+                        return new FolderTreeRow()
+                        {
+                            id = (reader["id"] as int?) ?? -1,
+                            folderName = reader["name"] as string,
+                            parentid = reader["parentid"] as int? ?? -1,
+                            error = false
+                        };
+                    }
+                );
+
+            // One id is one folder!!
+            if (rows.Count == 1)
+                result = rows[0];
+            else if (rows.Count > 1)
+                throw new Exception("Multiple folder with same rowid?");
+
+            return result;
+        }
+
+        public SQLUtils.SQLCommand SQL_FOLDERTREE_selectByParentAndName =
+           new SQLUtils.SQLCommand(
+               @"SELECT * FROM FolderTree WHERE parentid = @parentid AND name = @name",
+               new List<SQLUtils.SQLCommand.SQLParam>()
+               {
+                    new SQLUtils.SQLCommand.SQLParam("@parentid", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
+                    new SQLUtils.SQLCommand.SQLParam("@name", SQLUtils.SQLCommand.SQLParam.sqliteType.TEXT)
+               });
+
+        FolderTreeRow findFolderByParentAndName(int parentid, string partname)
+        {
+            FolderTreeRow result = new FolderTreeRow();
+
+            List<FolderTreeRow> rows = new List<FolderTreeRow>();
+            rows = SQL_FOLDERTREE_selectByParentAndName.ExecuteReadAll<FolderTreeRow>(
+                    new List<object>() { parentid, partname },
+                    (reader) => {
+                        return new FolderTreeRow()
+                        {
+                            id = (reader["id"] as int?) ?? -1,
+                            folderName = reader["name"] as string,
+                            parentid = reader["parentid"] as int? ?? -1,
+                            error = false
+                        };
+                    }
+                );
+
+            // Like real folders only one unique name under each parent!
+            if (rows.Count == 1)
+                result = rows[0];
+            else if (rows.Count > 1)
+                throw new Exception("Multiple folder with same name under same parent?");
+
+            return result;
+        }
+
+        public SQLUtils.SQLCommand SQL_FOLDERTREE_lastSequence =
+           new SQLUtils.SQLCommand(
+               @"SELECT seq FROM sqlite_sequence WHERE name = 'FolderTree'",
+               null);
+
+        int getLastSequence()
+        {
+            List<int> seq =
+                SQL_FOLDERTREE_lastSequence.ExecuteReadAll<int>(
+                    null,
+                    (reader) => { return (reader["seq"] as int?) ?? -1; }
+                    );
+
+            if (seq.Count != 1) throw new Exception("Can't read sequence counter. try after insert/delete");
+            return seq[0];
+        }
+
+
+
     }
 }
--- PlasticBackupDB/SQLData/SQLQueries.cs
+++ PlasticBackupDB/SQLData/SQLQueries.cs
@@ -5,27 +5,9 @@ using System.Text;
 
 namespace PlasticBackupDB.SQLData
 {
-    static class SQLQueries
+    static class SQLQueriesRaw
     {
         public const string GET_ALL_TABLES_RAW = @"SELECT name FROM sqlite_master WHERE type='table';";
 
-        // FolderTree
-        public static SQLUtils.SQLCommand FOLDERTREE_insert =
-            new SQLUtils.SQLCommand(
-                @"INSERT INTO FolderTree (parentid, name) VALUES (@parentid,@name)",
-                new List<SQLUtils.SQLCommand.SQLParam>()
-                {
-                    new SQLUtils.SQLCommand.SQLParam("@parentid", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
-                    new SQLUtils.SQLCommand.SQLParam("@name", SQLUtils.SQLCommand.SQLParam.sqliteType.TEXT)
-                });
-
-        public static SQLUtils.SQLCommand FOLDERTREE_selectById =
-            new SQLUtils.SQLCommand(
-                @"SELECT * FROM FolderTree WHERE id = @id",
-                new List<SQLUtils.SQLCommand.SQLParam>()
-                {
-                    new SQLUtils.SQLCommand.SQLParam("@id", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
-                });
-
-    }
+     }
 }
