7a42e2893cdcddba77948de8892819c9d20b9f35
yonixw <yonixw@gmail.com>
2017-11-22 22:06:36 +0200

    not TEMP tables.

--- PlasticBackupDB/SQLTables/SQLQueries.cs
+++ PlasticBackupDB/SQLTables/SQLQueries.cs
@@ -7,6 +7,6 @@ namespace PlasticBackupDB.SQLTables
 {
     static class SQLQueries
     {
-        public const string GET_ALL_TABLES = @"SELECT name FROM sqlite_temp_master WHERE type='table';";
+        public const string GET_ALL_TABLES = @"SELECT name FROM sqlite_master WHERE type='table';";
     }
 }
