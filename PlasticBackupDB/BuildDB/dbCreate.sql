f3ebdaa32f2e69b01e59fa3d9d6cfb3d00f8489a
yonixw <yonixw@gmail.com>
2017-11-22 23:04:35 +0200

    adding autoincrement to be able to read last id from sqlite_sequence

--- PlasticBackupDB/BuildDB/dbCreate.sql
+++ PlasticBackupDB/BuildDB/dbCreate.sql
@@ -1,12 +1,14 @@
+-- Autoincrement and sqlite_sequence https://stackoverflow.com/a/2217015/1997873
+
 CREATE TABLE FolderTree (
-    id        INTEGER PRIMARY KEY
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
                       NOT NULL,
     parentid  INTEGER NOT NULL,
     name      TEXT    NOT NULL
 );
 
 CREATE TABLE Files (
-    id        INTEGER PRIMARY KEY
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
                       NOT NULL,
     folderid  INTEGER NOT NULL,
     name      TEXT    NOT NULL
@@ -17,7 +19,7 @@ CREATE TABLE Files (
 -- New path of file is only determined by it's hash metadata. No duplicates!
 
 CREATE TABLE MetadataInstances (
-    id        INTEGER PRIMARY KEY
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
                       NOT NULL,
 	isFolder  BOOLEAN NOT NULL,
     sourceid  INTEGER NOT NULL -- Search in folder\file table based on `isFolder`
@@ -26,7 +28,7 @@ CREATE TABLE MetadataInstances (
 -- Each Instance have <key,value> pairs of metadata.
 
 CREATE TABLE MetadataItems (
-	id        INTEGER PRIMARY KEY
+	id        INTEGER PRIMARY KEY AUTOINCREMENT
                       NOT NULL,
 	instanceid       INTEGER NOT NULL,
     metakey          TEXT    NOT NULL,
