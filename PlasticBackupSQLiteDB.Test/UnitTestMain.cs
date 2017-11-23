a3b5030ca171b40b77c9eed6b794e36252e68563
yonixw <yonixw@gmail.com>
2017-11-23 02:08:47 +0200

    added test for making subfolders and getting subfolders.

--- PlasticBackupSQLiteDB.Test/UnitTestMain.cs
+++ PlasticBackupSQLiteDB.Test/UnitTestMain.cs
@@ -63,5 +63,47 @@ namespace PlasticBackupSQLiteDB.Test
             Assert.AreEqual(folder.id, folder2.id);
 
         }
+
+        [TestMethod]
+        public void AddSubfoldersAndCheckThem()
+        {
+            FolderTree FolderTreefunc = new FolderTree(conn);
+
+            List<string> testPath = new List<string>();
+            testPath.AddRange(new[] { "MY-PC2", "D:", "SubfolderTest"});
+
+            FolderTree.FolderTreeRow folder = FolderTreefunc.createOrFindFolder(testPath);
+
+            Assert.IsNotNull(folder);
+
+            // Check for errors.
+            Assert.IsTrue(folder.error == false);
+
+            // Create subfolders:
+            FolderTree.FolderTreeRow sub1 = FolderTreefunc.createOrFindChildFolder(folder, "Sub1");
+            FolderTree.FolderTreeRow sub2 = FolderTreefunc.createOrFindChildFolder(folder, "Sub2");
+            Assert.AreEqual(sub1.parentid, sub2.parentid);
+
+            // Get SubFolders:
+            List<FolderTree.FolderTreeRow> subfolders = FolderTreefunc.getSubFolders(folder);
+            Assert.IsTrue(subfolders.Count == 2); 
+
+            // See if subfolders are the same :
+            Assert.IsTrue(
+                /*Option 1 : s1 = s[0] && s2 = s[1]*/
+                (sub1.id == subfolders[0].id && sub2.id == subfolders[1].id)
+                ||
+                /*Option 2 : reversed */
+                (sub1.id == subfolders[1].id && sub2.id == subfolders[0].id)
+                );
+
+            // Check we get subfolder with other methods:
+            List<string> testPath2 = new List<string>();
+            testPath2.AddRange(new[] { "MY-PC2", "D:", "SubfolderTest", "Sub2" });
+            FolderTree.FolderTreeRow sub2Path = FolderTreefunc.createOrFindFolder(testPath2);
+
+            Assert.AreEqual(sub2Path.id, sub2.id);
+
+        }
     }
 }
