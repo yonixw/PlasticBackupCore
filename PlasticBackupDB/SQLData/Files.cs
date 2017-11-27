4e8af31147a9dd86113106f0a893e30094841196
yonixw <yonixw@gmail.com>
2017-11-27 21:16:20 +0200

    interface of table is set. now to write a test (ignore the wrong SQL_FILES_selectByParentID)

--- PlasticBackupDB/SQLData/Files.cs
+++ PlasticBackupDB/SQLData/Files.cs
@@ -20,8 +20,22 @@ namespace PlasticBackupDB.SQLData
             public bool error = true; // This class has invalid information.
         }
 
-        public List<FileRow> getFolderFiles(FolderTree.FolderTreeRow folder) { return null; }
+
+        public SQLUtils.SQLCommand SQL_FILES_selectByParentID =
+            new SQLUtils.SQLCommand(
+                @"INSERT INTO FolderTree (parentid, name) VALUES (@parentid, @name)",
+                new List<SQLUtils.SQLCommand.SQLParam>()
+                {
+                    new SQLUtils.SQLCommand.SQLParam("@parentid", SQLUtils.SQLCommand.SQLParam.sqliteType.INTEGER),
+                    new SQLUtils.SQLCommand.SQLParam("@name", SQLUtils.SQLCommand.SQLParam.sqliteType.TEXT)
+                });
 
         public FileRow createOrFindFile(FolderTree.FolderTreeRow folder, string filename) { return null; }
+
+        public FileRow findFileById(long id) { return null; }
+
+        public void deleteFile(FileRow file) {  }
+
+        public List<FileRow> getFolderFiles(FolderTree.FolderTreeRow folder) { return null; }
     }
 }
